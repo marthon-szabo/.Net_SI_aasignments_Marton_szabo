04b5f414ccfd2909b76f68208282af9bafd80a97
marthon-szabo <makotoimx@gmail.com>
2020-05-25 10:52:20 +0200

    Initialize Person class

---
+++ 1st_week/Exercise_1-2/Exercise_1-2/Application.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Exercise_1_2
+{
+    public class Application
+    {
+        public static void Main(string[] args)
+        {
+            Person person1 = new Person("Jason Smith", new DateTime(1980, 12,03));
+            
+        }
+    }
+}
---
+++ 1st_week/Exercise_1-2/Exercise_1-2/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise_1_2
+{
+    internal class Person
+    {
+        string name;
+        DateTime birthDate;
+        
+
+        public Person(string name, DateTime birthDate)
+        {
+            this.name = name;
+            this.birthDate = birthDate;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public DateTime BirthDate => birthDate;
+
+        public override string ToString()
+        {
+            return $"Name: {name}, birth: {birthDate}.";
+        }
+    }
+}
