eafe290c5d523754463fec62b911ac3667a4ef1e
Márton Szabó <makotoimx@gmail.com>
2020-05-26 11:14:21 +0200

    Add SortString excersise

---
+++ 1st_week/Exercise_1-2/SortString(Exc._5)/SortString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace SortString_Exc._5_
+{
+    internal class SortString
+    {
+        public static void Main(string[] args)
+        {
+            string str = "First try, but not last.";
+            string[] words = str.Split(" ");
+            Array.Sort(words);
+            words.ToList().ForEach(i => Console.WriteLine(i.ToString()));
+            string s = string.Join(" ", words);
+            Console.WriteLine(s);
+        }
+    }
+}
