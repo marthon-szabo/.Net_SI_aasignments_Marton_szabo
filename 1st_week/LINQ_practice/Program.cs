13bceec33397c90c7015f12d63f1a3ba8081150e
Márton Szabó <makotoimx@gmail.com>
2020-05-31 17:45:22 +0200

    Initialize LINQ practice

---
+++ 1st_week/LINQ_practice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
