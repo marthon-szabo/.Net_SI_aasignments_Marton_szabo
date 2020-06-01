2fe3c4d0a207ad8c8b8ee84a765c4572815df47e
Márton Szabó <makotoimx@gmail.com>
2020-06-01 15:03:18 +0200

    Initialize Own_Snake project

---
+++ 1st_week/LINQ_practice/LINQProgram.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_practice
+{
+    class LINQProgram
+    {
+        static void Main(string[] args)
+        {
+            var startingDeck = from k in Suits()
+                               from z in Ranks()
+                               select new { Kartya = k, Rang = z };
+
+            foreach (var card in startingDeck)
+            {
+                Console.WriteLine(card);
+            }
+        }
+            static IEnumerable<string> Suits()
+            {
+                yield return "clubs";
+                yield return "diamonds";
+                yield return "hearts";
+                yield return "spades";
+            }
+
+            static IEnumerable<string> Ranks()
+            {
+                yield return "two";
+                yield return "three";
+                yield return "four";
+                yield return "five";
+                yield return "six";
+                yield return "seven";
+                yield return "eight";
+                yield return "nine";
+                yield return "ten";
+                yield return "jack";
+                yield return "queen";
+                yield return "king";
+                yield return "ace";
+            }
+    }
+}
---
+++ 1st_week/Own_Snake/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Own_Snake
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int snakeSize = 20;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DrawSnake();
+        }
+
+        private void DrawSnake()
+        {
+            byte rowNumber = 1;
+            bool isDone = false;
+            bool isOdd = true;
+
+            while (isDone == false)
+            {
+                UIElement rectangle = new Rectangle()
+                {
+                    Width = snakeSize,
+                    Height = snakeSize,
+                    Fill = isOdd ? Brushes.Black : Brushes.White
+                };
+
+            }
+        }
+    }
+}
