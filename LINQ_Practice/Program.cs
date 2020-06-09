eb0952a0c8e57fc4981b50bcbdeceddcdbcba126
Márton Szabó <makotoimx@gmail.com>
2020-06-09 11:44:57 +0200

    Practice with LINQ

--- LINQ_Practice/Program.cs
+++ LINQ_Practice/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
 
 namespace LINQ_Practice
 {
@@ -6,7 +9,30 @@ namespace LINQ_Practice
     {
         static void Main(string[] args)
         {
-            
+
+            var locomotives = from name in Names()
+                              from loc in Locs()
+                              where loc.Equals("V63")
+                              select new { Name = "Gigant" };
+
+            foreach (var e in locomotives)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static IEnumerable<string> Locs()
+        {
+            yield return "V63";
+            yield return "V43";
+            yield return "M62";
+        }
+
+        static IEnumerable<string> Names()
+        {
+            yield return "Gigant";
+            yield return "Szili";
+            yield return "Csorgo";
         }
     }
 }
