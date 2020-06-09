16c4c6bf3f362d3354977351a7c95c8c2613338b
Márton Szabó <makotoimx@gmail.com>
2020-06-09 10:31:16 +0200

    Add LocomEnum class

---
+++ 2nd_SI/LocomCollection.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Transactions;
+
+namespace _2nd_SI
+{
+    class LocomCollection : System.Collections.IEnumerable
+    {
+        Locomotive[] locoms = new Locomotive[] { new Locomotive("V43"), new Locomotive("V63") };
+
+        public LocomCollection(Locomotive[] locoms)
+        {
+            this.locoms = locoms;
+        }
+
+        sbyte position = -1;
+
+     
+
+        public IEnumerator GetEnumerator()
+        {
+        }
+    }
+
+    internal class LocomEnum : IEnumerator
+    {
+        Locomotive[] locoms;
+        sbyte position = -1;
+
+        public LocomEnum(Locomotive[] locoms)
+        {
+            this.locoms = locoms;
+        }
+
+        public object Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < locoms.Length);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+
+    internal class Locomotive
+    {
+        public string Name { get; }
+
+        public Locomotive(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ 2nd_SI/Practice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _2nd_SI
+{
+    class Practice
+    {
+        
+        static async Task Main(string[] args)
+        {
+            
+        }
+
+        
+
+       
+        
+        
+    }
+}
