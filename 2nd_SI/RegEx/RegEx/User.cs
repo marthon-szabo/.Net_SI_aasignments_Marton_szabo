4cb25b3b44dde0ca2d3d72849404bbc2b2d03ebb
Márton Szabó <makotoimx@gmail.com>
2020-06-11 23:02:38 +0200

    Initialiye User class

---
+++ 2nd_SI/RegEx/RegEx/User.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegEx
+{
+    class User : INotifyPropertyChanged
+    {
+        #region Fields
+        string _name;
+        string _phone;
+        string _eMail;
+        #endregion
+
+        #region Properties
+        public String Name
+        {
+            get { return _name; }
+
+            set
+            {
+                if (!_name.Equals(value))
+                {
+                    _name = value;
+                    NotifyChange("Name");
+                }
+            }
+        }
+        
+        public String Phone
+        {
+            get { return _phone; }
+
+            set
+            {
+                if (!_phone.Equals(value))
+                {
+                    _phone = value;
+                    NotifyChange("Phone");
+                }
+            }
+        }
+        
+        public String Email
+        {
+            get { return _eMail; }
+
+            set
+            {
+                if (!_eMail.Equals(value))
+                {
+                    _eMail = value;
+                    NotifyChange("Phone");
+                }
+            }
+        }
+
+        #endregion
+
+        #region InotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyChange(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
